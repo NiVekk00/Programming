cc52d61850179f8a4cecb4ef312f04661a8d3df4
NiVek <70785507+NiVekk00@users.noreply.github.com>
2022-03-30 19:59:38 +0200

    Calculator v1.0 30.03.2022
    Very good

--- School/K/Calculator/script.js
+++ School/K/Calculator/script.js
@@ -1,41 +1,49 @@
-const log = console.log
+const log = console.log     // Dla sportu
 
-var Number = " ";
-var result = 0;
-var operator = " ";
+var num = " ";
+var result = 0;             // Deklaracja podstawowych zmiennych
 var show = " ";
+var counter = 0;
 
 function Add(value){
 
     var Screen = document.getElementById("screen");
-    Number += value;
-    show += value.toString()
+    num += value;
+    show += value.toString()                                // Funkcja przycisków, która dodaje kolejną wartośc do stinga + wyświetla **value** danego przycisku
 
     Screen.innerHTML = show.toString();
 }
 
-function Sum(value){
+function Operator(value){
 
     var Screen = document.getElementById("screen");
-    //var operator = value;
-    Screen.innerHTML = value;
-    Number += value;
+    Screen.innerHTML = " ";                                          // Funkcja, która po wciśnięciu znaku pokazuje puste pole
+
+    if(num[num.length-1] == "+" || num[num.length-1] == "-" || num[num.length-1] == "/" || num[num.length-1] == "*"){  
+        num.slice(0, num.length - 1)
+    }
+
+    num += value;       // Dodanie znaku do stringu
+    counter++;   
     show = " ";
+
+    if(counter >= 2){
+        log(num.slice(0, num.length - 1))               // Operator if, który po wciśnięciu jakiegoś znaku pokazuje wynik (nie trzeba klikać znaku "=")
+        Screen.innerHTML = eval(num.slice(0, num.length - 1));
+    }
 }
 
 function Calc(){
 
     var Screen = document.getElementById("screen");
-    log(Number)
-
-    Screen.innerHTML = eval(Number)
+                                                                // Funkcja, która po wciśnięciu znaku "=" wyświetla gotowy wynik
+    Screen.innerHTML = eval(num)
 }
 
 function Clear(){
     var Screen = document.getElementById("screen");
-    Number = " ";
-    result = 0;
-    operator = " ";
+    num = " ";
+    result = 0;                                             // Funkcja czyszcząca wszystkie zmienne (przycisk "CE") oraz wyświetla 0 na ekranie
     show = " ";
 
     Screen.innerHTML = "0"
