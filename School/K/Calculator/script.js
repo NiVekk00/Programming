d795e5d71c9f906a53545904ad88dd8482a84101
NiVek <70785507+NiVekk00@users.noreply.github.com>
2022-03-31 19:48:31 +0200

    31.03.2022
    Calculator v1.5

--- School/K/Calculator/script.js
+++ School/K/Calculator/script.js
@@ -1,34 +1,31 @@
 const log = console.log     // Dla sportu
 
-var num = " ";
-var result = 0;             // Deklaracja podstawowych zmiennych
-var show = " ";
+var num = " ";             
+var show = " ";                            // Deklaracja podstawowych zmiennych
 var counter = 0;
 
 function Add(value){
 
-    var Screen = document.getElementById("screen");
+    var Screen = document.getElementById("screen");    // Deklaracja divu ekran
     num += value;
-    show += value.toString()                                // Funkcja przycisków, która dodaje kolejną wartośc do stinga + wyświetla **value** danego przycisku
+    show += value                                // Funkcja przycisków, która dodaje kolejną wartośc do stinga + wyświetla **value** danego przycisku
 
-    Screen.innerHTML = show.toString();
+    Screen.innerHTML = show;
 }
 
 function Operator(value){
 
     var Screen = document.getElementById("screen");
-    Screen.innerHTML = " ";                                          // Funkcja, która po wciśnięciu znaku pokazuje puste pole
-
-    if(num[num.length-1] == "+" || num[num.length-1] == "-" || num[num.length-1] == "/" || num[num.length-1] == "*"){  
-        num.slice(0, num.length - 1)
-    }
+    Screen.innerHTML = " ";                                     // Funkcja, która po wciśnięciu znaku pokazuje puste pole
 
     num += value;       // Dodanie znaku do stringu
-    counter++;   
+    counter++;          
     show = " ";
 
     if(counter >= 2){
-        log(num.slice(0, num.length - 1))               // Operator if, który po wciśnięciu jakiegoś znaku pokazuje wynik (nie trzeba klikać znaku "=")
+
+        log(num.slice(0, num.length - 1))
+                                                    // Operator if, który po wciśnięciu jakiegoś znaku pokazuje wynik (nie trzeba klikać znaku "=")
         Screen.innerHTML = eval(num.slice(0, num.length - 1));
     }
 }
@@ -42,8 +39,7 @@ function Calc(){
 
 function Clear(){
     var Screen = document.getElementById("screen");
-    num = " ";
-    result = 0;                                             // Funkcja czyszcząca wszystkie zmienne (przycisk "CE") oraz wyświetla 0 na ekranie
+    num = " ";                                             // Funkcja czyszcząca wszystkie zmienne (przycisk "CE") oraz wyświetla 0 na ekranie
     show = " ";
 
     Screen.innerHTML = "0"
